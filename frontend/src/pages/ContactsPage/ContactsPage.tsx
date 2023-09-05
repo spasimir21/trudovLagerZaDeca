66e0be9f765219cd0231363bab0dabb56c65d779
BozhidarLazarov <124928170+BozhidarLazarov@users.noreply.github.com>
2023-09-06 01:45:18 +0300

    Make Contacts Framework Page

--- frontend/src/pages/ContactsPage/ContactsPage.tsx
+++ frontend/src/pages/ContactsPage/ContactsPage.tsx
@@ -1,4 +1,5 @@
-import { HeaderSpacing } from '../../components/HeaderSpacing/HeaderSpacing';
+import { HeaderSpacing} from '../../components/HeaderSpacing/HeaderSpacing';
+import { PersonalInformationCard } from '../../components/PersonalInformationCard/PersonalInformationCard';
 // @ts-ignore
 import * as classes from './ContactsPage.module.scss';
 import React from 'react';
@@ -7,8 +8,49 @@ function ContactsPage() {
   return (
     <div className='page'>
       <HeaderSpacing />
-      <div className={classes.section}></div>
-    </div>
+      {/* <div className={classes.section}></div> */}
+      <div className="titleContainer">
+        <p>Контакти</p>
+      </div>
+
+      <div className='moreInformationContainer'>
+
+        <p>
+        За въпроси и повече информация - Свържете се с нас!
+        </p>
+
+        <ul>data</ul>
+        <ul>data</ul>
+      </div>
+
+      <div className={classes.youCanFindUsHereContainer}>
+        <p>Можете да ни намерите тук:</p>
+        <ul>data</ul>
+        <ul>data</ul>
+        <ul>data</ul>
+      </div>
+
+      <div className="titleContainer">
+        <p>ЕкипЪ</p>
+      </div>
+
+
+      <div className={classes.teamCardsContainer}>
+          <PersonalInformationCard/>
+          <PersonalInformationCard/>
+          <PersonalInformationCard/>
+          <PersonalInformationCard/>
+          
+          <PersonalInformationCard/>
+          <PersonalInformationCard/>
+          <PersonalInformationCard/>
+          <PersonalInformationCard/>
+
+      </div>
+
+      
+
+      </div>
   );
 }
 
