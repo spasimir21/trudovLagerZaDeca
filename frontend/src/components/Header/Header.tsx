6a10561df2d140a06cdb45b7f4088c761cfb7ff5
Spasimir Pavlov <spasimir.pavlov@gmail.com>
2023-09-05 23:34:35 +0300

    Updated the header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -4,18 +4,16 @@ import React from 'react';
 
 function Header() {
   return (
-    <div className={classes.header + ' ' + classes.full}>
-      <div className={classes.line}></div>
-      <div className={classes.logo}>
-        <div className={classes.leftButtons}>
-          <p>Начало</p>
-          <p>Контакти</p>
-          <p>Лагер</p>
-        </div>
-        <div className={classes.rightButtons}>
-          <p className={classes.button}>Запиши се</p>
-          <p className={classes.button + ' ' + classes.loginButton}>Log in</p>
-        </div>
+    <div className={classes.header}>
+      <div className={classes.buttons}>
+        <div className={classes.logo}></div>
+        <p>Начало</p>
+        <p>Контакти</p>
+        <p>Лагер</p>
+      </div>
+      <div className={classes.buttons}>
+        <p className={classes.button}>Запиши се</p>
+        <p className={classes.button + ' ' + classes.accentButton}>Вход</p>
       </div>
     </div>
   );
